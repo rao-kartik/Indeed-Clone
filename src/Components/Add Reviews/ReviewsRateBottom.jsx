ce7b3fce20f4a83bb14ff464309e1effc7706cf5
Ajmal KV <reallyaju@gmail.com>
2021-04-16 08:57:02 +0530

    Added review rating bottom

---
+++ src/Components/Add Reviews/ReviewsRateBottom.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { RatingButton } from '../../Custom UI/ACustomUI';
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+    font-size:19.95px;
+    margin:0px 0px 15px;
+    padding:24px 0px 0px;
+`;
+export const ReviewsRateBottom=({compname})=>{
+    return(
+        <div>
+                <H1>Please rate the level of difficulty of your interview at {compname}.</H1>
+                <RatingButton>1</RatingButton>
+                <RatingButton>2</RatingButton>
+                <RatingButton>3</RatingButton>
+                <RatingButton>4</RatingButton>
+                <RatingButton>5</RatingButton>
+            </div>
+    )
+}

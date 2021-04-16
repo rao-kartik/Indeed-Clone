927c254214376cfa11f3ba0a9ee8395ee532835b
Ajmal KV <reallyaju@gmail.com>
2021-04-16 14:45:11 +0530

    Edited Styles

--- src/Components/CompanyReviews/CompanySearch.jsx
+++ src/Components/CompanyReviews/CompanySearch.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect,useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
 import {Button,Input,H1,P,Label} from '../../Custom UI/ACustomUI';
 import style from './CompanySearch.module.css';
 import Select from 'react-select'
@@ -11,11 +11,12 @@ export const CompanySearch=()=>{
     useEffect(()=>{
         document.title = 'Find the Best Companies Hiring Now | Indeed.com';
     });
-    const options = [
-        { value: 'Amazon', label: 'Amazon' },
-        { value: 'Google', label: 'Google' },
-        { value: 'Facebook', label: 'Facebook' }
-      ]
+    // const options = [
+    //     { value: 'Amazon', label: 'Amazon' },
+    //     { value: 'Google', label: 'Google' },
+    //     { value: 'Facebook', label: 'Facebook' }
+    //   ]
+    const[options,setOptions] = React.useState([]);
       const dispatch = useDispatch();
   const { isLoading, isError,companies } = useSelector(
     (state) => state.company,
@@ -30,7 +31,7 @@ export const CompanySearch=()=>{
       .then((res) => {
         const successAction = companiesSuccess(res.data);
         dispatch(successAction);
-        console.log(res.data);
+        // console.log(res.data);
       })
       .catch((err) => {
         const failureAction = companiesFailure(err);
@@ -39,8 +40,15 @@ export const CompanySearch=()=>{
   };
   useEffect(()=>{
       getData();
+    //   setOptions()
+    setOptions(companies.map((element)=>({value:element.company_name, label: element.company_name})));
       
   },[])
+  const [searchText,setSearchText] = useState('');
+  const history = useHistory();
+  const handleSearch=()=>{
+      history.push(`/companies/search/${searchText}`);
+  }
     return(
         <div className={style.container}>
             <H1>Find great places to work</H1>
@@ -49,19 +57,20 @@ export const CompanySearch=()=>{
                 <div>
                 {/* <Label>
                     Company name or job title */}
-                    <Input className={style.inputCompany} placeholder='Enter a company name'/><br/>
-                    <Link style={{ textDecoration: 'none',color:'#085ff7',fontSize:'13px' }}>Do you want to search for salaries?</Link>
+                    <Input className={style.inputCompany} onChange={(e)=>setSearchText(e.target.value)} placeholder='Enter a company name'/><br/>
+                    <Link to='/career/salaries' style={{ textDecoration: 'none',color:'#085ff7',fontSize:'13px' }}>Do you want to search for salaries?</Link>
+                    {/* <Select options={options} placeholder='Enter a company name' components={{ DropdownIndicator:() => null, IndicatorSeparator:() => null }} style={{width:'150px'}}/> */}
                 {/* </Label> */}
                 </div>
                 <div>
                 {/* <Label>
                     City, state, or zip (optional)
                     <Input className={style.inputLocation}/>
-                    {/* <Select options={options} placeholder='Enter a company name' components={{ DropdownIndicator:() => null, IndicatorSeparator:() => null }} style={{width:'150px'}}/> */}
+                     
                 {/* </Label> */}
                 </div>
                 <div style={{marginTop:'1%',textAlign:'center'}}>
-                    <Button>Find Companies</Button>
+                    <Button onClick={handleSearch}>Find Companies</Button>
                     
                 </div>
             </div>           

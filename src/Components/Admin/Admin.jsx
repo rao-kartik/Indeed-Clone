bb17e5c704d1f9522fe133d11416509363e37628
Kartik Yadav <kartikyadav94@gmail.com>
2021-04-16 15:00:57 +0530

    added admin page and fetched jobs and recruiters data

---
+++ src/Components/Admin/Admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AdminLogin } from './AdminLogin';
+import styled from 'styled-components';
+import { AdminNavbar } from './AdminNavbar';
+
+const Container = styled.div`
+
+`;
+
+export const Admin = () => {
+    return (
+        <Container>
+            {/* <AdminLogin /> */}
+        </Container>
+    )
+}

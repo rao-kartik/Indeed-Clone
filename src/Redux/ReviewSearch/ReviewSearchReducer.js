3cfba6c4e7fb515177c984601a16db064156cbfd
Ajmal KV <reallyaju@gmail.com>
2021-04-16 14:50:07 +0530

    Redux files for Review Search

---
+++ src/Redux/ReviewSearch/ReviewSearchReducer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { SEARCH_REQUEST, SEARCH_SUCCESS, SEARCH_FAILURE } from './actionTypes';
+
+const initState = {
+    isLoading: false,
+    isError: false,
+    data: []
+}
+
+export const ReviewSearchReducer = (state=initState, action) => {
+    const { payload } = action;
+    switch (action?.type) {
+        case SEARCH_REQUEST: {
+            return {
+                ...state,
+                isLoading: true,
+                isError: false
+            }
+        }
+        case SEARCH_SUCCESS: {
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: payload
+            }
+        }
+        case SEARCH_FAILURE: {
+            return {
+                ...state,
+                isLoading: false,
+                isError: true
+            }
+        }
+    
+        default:
+            return state;
+    }
+}

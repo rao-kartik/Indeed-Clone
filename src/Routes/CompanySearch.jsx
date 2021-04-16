7cd7a2fce7af8746cc287c2046e0277cbe43e4e4
Ajmal KV <reallyaju@gmail.com>
2021-04-16 14:50:37 +0530

    Company search route file

---
+++ src/Routes/CompanySearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { CompanySearchPage } from '../Components/CompanyReviews/CompanySearchPage';
+export const CompanySearch=()=>{
+    return(
+        <div>
+            <CompanySearchPage/>
+        </div>
+    )
+}

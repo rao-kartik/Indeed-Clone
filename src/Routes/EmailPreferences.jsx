aa12076261eea3a6c545f208ddcb2b8159b8fbe7
Robin Singh <Rskamra002@gmail.com>
2021-04-18 14:23:59 +0530

    Routing Error Fixed

--- src/Routes/EmailPreferences.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { EmailPreferences } from '../Components/Email Preferences/EmailPreferences'
-export const EmailPreferences = () => {
-    return (
-        <EmailPreferences />
-    )
-}

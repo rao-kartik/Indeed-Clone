3c1adf16106391d1d2944faac36f0ae2de7df652
Robin Singh <Rskamra002@gmail.com>
2021-04-18 14:08:09 +0530

    Routes Update

---
+++ src/Routes/EmailPreferences.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { EmailPreferences } from '../Components/Email Preferences/EmailPreferences'
+export const EmailPreferences = () => {
+    return (
+        <EmailPreferences />
+    )
+}

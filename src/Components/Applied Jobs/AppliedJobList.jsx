622627ed5dd6f2e3f31b0f9032b00dbd24b8db19
Ajmal KV <reallyaju@gmail.com>
2021-04-19 09:06:50 +0530

    Changed API endpoints

--- src/Components/Applied Jobs/AppliedJobList.jsx
+++ src/Components/Applied Jobs/AppliedJobList.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router';
+import { Redirect, useHistory } from 'react-router';
 import { deleteJob } from '../../Redux/MyJobs/action';
 import { AppliedJobListItem } from './AppliedJobListItem';
 
 export const AppliedJobList=({reviewsData})=>{
     const dispatch = useDispatch();
     const history = useHistory();
+    const [isRefresh,setIsRefresh] = useState(true);
     const handleDelete = (id) =>{
         dispatch(deleteJob(id))
-        history.push('/appliedjobs')
+        setIsRefresh(false)
     }
-    return(
+    return isRefresh?(
         <div>
             {
                 reviewsData.map(item=><AppliedJobListItem key={item.id} {...item} handleDelete={handleDelete}/>)
             }
         </div>
-    )
+    ):(<Redirect to='/appliedjobs'/>)
 }

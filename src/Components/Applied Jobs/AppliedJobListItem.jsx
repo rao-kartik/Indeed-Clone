92305b83b42b2dc6069ffc3666da4000a5f6b7af
Ajmal KV <reallyaju@gmail.com>
2021-06-09 12:12:27 +0530

    Applied jobs - formatting

--- src/Components/Applied Jobs/AppliedJobListItem.jsx
+++ src/Components/Applied Jobs/AppliedJobListItem.jsx
@@ -1,48 +1,50 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
-import { jobsByIdSearch } from '../../Redux/FindJobById/action';
-// import { deleteReview } from '../../Redux/MyReviews/action';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { jobsByIdSearch } from "../../Redux/FindJobById/action";
 
 const Div = styled.div`
-    border:1px solid #085ff7;
-    width:100%;
-    height:100%;
-    margin:auto;
-    margin-top:10px;
-    padding:20px;
-    border-radius:15px;
-    -webkit-box-shadow: 0 0 5px #085ff7;
-        box-shadow: 0 0 5px #085ff7;
-        position: relative;
+  border: 1px solid #085ff7;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  margin-top: 10px;
+  padding: 20px;
+  border-radius: 15px;
+  -webkit-box-shadow: 0 0 5px #085ff7;
+  box-shadow: 0 0 5px #085ff7;
+  position: relative;
 `;
 const Container = styled.div`
-    padding-left:30px;
-    color:#085ff7;
+  padding-left: 30px;
+  color: #085ff7;
 `;
 const Delete = styled.div`
-    position: absolute;
-    right: 20px;
-    bottom: 10px;
+  position: absolute;
+  right: 20px;
+  bottom: 10px;
 `;
-export const AppliedJobListItem=({email, jobid,id,handleDelete})=>{
-    const jobsdata = useSelector(state=>state.jobsByIdReducer.jobs_data)
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(jobsByIdSearch(jobid))
-    },[])
-    console.log(jobsdata);
-    return(
-        <Div>
-            <h1>{jobsdata.title}</h1>
-            <h1 style={{fontStyle:'italic'}}>{jobsdata.company_name}</h1>
-            <Container>
-            <p>Category : {jobsdata.category}</p>
-            <p>{jobsdata.description}</p>
-            <p>Review by {jobsdata.location} - {jobsdata.salary}</p>
-            </Container>
-            <p>{jobsdata.publication_date}</p>
-            <Delete onClick={()=>handleDelete(id)}><span class="material-icons-outlined">delete</span></Delete>
-        </Div>
-    )
-}
+export const AppliedJobListItem = ({ email, jobid, id, handleDelete }) => {
+  const jobsdata = useSelector((state) => state.jobsByIdReducer.jobs_data);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(jobsByIdSearch(jobid));
+  }, []);
+  return (
+    <Div>
+      <h1>{jobsdata.title}</h1>
+      <h1 style={{ fontStyle: "italic" }}>{jobsdata.company_name}</h1>
+      <Container>
+        <p>Category : {jobsdata.category}</p>
+        <p>{jobsdata.description}</p>
+        <p>
+          Review by {jobsdata.location} - {jobsdata.salary}
+        </p>
+      </Container>
+      <p>{jobsdata.publication_date}</p>
+      <Delete onClick={() => handleDelete(id)}>
+        <span class="material-icons-outlined">delete</span>
+      </Delete>
+    </Div>
+  );
+};
